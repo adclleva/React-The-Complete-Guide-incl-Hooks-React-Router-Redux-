5ab58b8847ecd4b0ded0a670bebdd09ef1b3cde7
adclleva <arvin.lleva@yahoo.com>
2022-06-16 13:58:34 -0700

    added basic.ts file

---
+++ Section 27 - React + Typescript/basics.ts
@@ -0,0 +1,13 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+// we use the primitive type (lowercase - ex: number, string)
+let age: number = 24;
+
+age = 23;
+
+let userName: string;
+
+let isInsctructor: boolean;
