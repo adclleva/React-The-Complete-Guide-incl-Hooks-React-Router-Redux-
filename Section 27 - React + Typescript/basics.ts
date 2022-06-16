c5a81ccc047e67a11134c3b3e8feb1a883e390f1
adclleva <arvin.lleva@yahoo.com>
2022-06-16 14:02:25 -0700

    ts with array and objects

--- Section 27 - React + Typescript/basics.ts
+++ Section 27 - React + Typescript/basics.ts
@@ -11,3 +11,34 @@ age = 23;
 let userName: string;
 
 let isInsctructor: boolean;
+
+// More complex types
+
+// array of strings
+let hobbies: string[];
+hobbies = ["a", "b"];
+
+// it could be anything but defeats the purpose
+let anything: any;
+
+// type to define an object, object type definition
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "bob",
+  age: 32,
+};
+
+// this would give an error
+// person = {
+//   isEmployee: true
+// }
+
+// type to define an array of objects
+let people: {
+  name: string;
+  age: number;
+}[];
