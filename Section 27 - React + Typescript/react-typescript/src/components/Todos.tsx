3bf0d1d698ec262d3448b290dc268a4aae857931
adclleva <arvin.lleva@yahoo.com>
2022-06-16 17:10:44 -0700

    working with props and ts

--- Section 27 - React + Typescript/react-typescript/src/components/Todos.tsx
+++ Section 27 - React + Typescript/react-typescript/src/components/Todos.tsx
@@ -1,10 +1,20 @@
-function Todos() {
+import React from "react";
+
+type Props = {
+  items?: string[];
+  children?: React.ReactNode;
+};
+
+// this type notation indicates that this function is a functional component type
+// React.FC is a generic type and we are using this React.FC feature to set the prop types with <{}>
+const Todos = (props: Props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn React</li>
+      {props.items?.map((item) => {
+        return <li key={item}>{item}</li>;
+      })}
     </ul>
   );
-}
+};
 
 export default Todos;
